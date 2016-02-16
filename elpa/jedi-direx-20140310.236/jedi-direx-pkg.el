36d5f3609cfc295ba2c4108af3067b71943e3def
Kyle <kdhaywar@ualberta.ca>
2016-02-16 16:52:22 -0700

    first commit, internet HERE LIES MY JANKY EMACS SETTINGS

---
+++ elpa/jedi-direx-20140310.236/jedi-direx-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "jedi-direx" "20140310.236" "Tree style source code viewer for Python buffer" '((jedi "0.1.2") (direx "0.1alpha")))
---
+++ elpa/no-easy-keys-1.0.2/no-easy-keys-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "no-easy-keys" "1.0.2" "Learn the proper Emacs movement keys" 'nil :authors '(("Dan Amlund Thomsen" . "dan@danamlund.dk")) :maintainer '("Dan Amlund Thomsen" . "dan@danamlund.dk") :url "http://danamlund.dk/emacs/no-easy-keys.html")
